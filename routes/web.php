cc01f12f3f26366106cfc129a8d65c27e5604d14
alireza158 <57532841+alireza158@users.noreply.github.com>
2026-06-02 14:53:58 +0330

    Add product images and PDF export

--- routes/web.php
+++ routes/web.php
@@ -444,6 +444,7 @@ use App\Http\Controllers\ProductControllerWeb;
 
 
 Route::get('/products', [ProductControllerWeb::class, 'index'])->name('products.index');
+Route::get('/products/pdf', [ProductControllerWeb::class, 'pdf'])->name('products.pdf');
 Route::get('/products/{slug}', [ProductControllerWeb::class, 'show'])->name('products.show');
 
 
